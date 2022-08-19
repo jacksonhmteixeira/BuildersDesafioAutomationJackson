886eb4e663e59b1dd751f4879ef1ac7afdb3e4fb
Jackson <jacksonhmteixeira@gmail.com>
2022-08-19 05:04:14 -0300

    INSERT - Inserindo novos cenarios.

--- src/test/java/com/builders/desafio/steps/TemperaturaSteps.java
+++ src/test/java/com/builders/desafio/steps/TemperaturaSteps.java
@@ -1,4 +1,22 @@
 package com.builders.desafio.steps;
 
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+import io.cucumber.java.pt.Dado;
+
 public class TemperaturaSteps {
+
+    @Dado("dado que estou na tela inicial")
+    public void dado_que_estou_na_tela_inicial() {
+    }
+
+    @Quando("preencher toda a tela de cadastro")
+    public void preencher_toda_a_tela_de_cadastro() {
+
+    }
+
+    @Entao("o aplicativo deve apresentar a tela de temperatura")
+    public void o_aplicativo_deve_apresentar_a_tela_de_temperatura() {
+
+    }
 }
